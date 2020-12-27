c1d22c55bfae63ef11b208531b9ba9a2e69b58d2
ihsanrabb <ihsanrabbani25@gmail.com>
2020-12-27 11:11:43 +0700

    create sign in and sign up

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css'
 import { Switch, Route } from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route exact path='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </>
   )
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,17 +1,16 @@
 import './collection-item.styles.scss'
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
-  <div className="collection-item">
-    <div 
-      className="image"
+  <div className='collection-item'>
+    <div
+      className='image'
       style={{
         backgroundImage: `url(${imageUrl})`
-      }}  
-    >
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
+      }}
+    />
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
     </div>
   </div>
 )

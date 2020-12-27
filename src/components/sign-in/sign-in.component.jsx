eab02444c05d99972aac3c28f69d182a9e05f4c2
ihsanrabb <ihsanrabbani25@gmail.com>
2020-12-27 18:49:14 +0700

    implement signIn with email

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -14,10 +14,15 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault()
-    alert(this.state.email)
-    this.setState({email: '', password: ''})
+    const {email, password} = this.state
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({email: '', password: ''})
+    } catch(err) {
+      console.log(err)
+    }
   }
 
   handleChange = event => {
@@ -50,7 +55,7 @@ class SignIn extends React.Component {
 
           <div className="buttons">
             <CustomButton type='submit'>Sign in</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Signin with google</CustomButton>
+            <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>Signin with google</CustomButton>
           </div>
         </form>
       </div>

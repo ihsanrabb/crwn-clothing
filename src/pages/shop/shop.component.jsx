91ecf92f940b829e1976ac2d1705d6e63f3da219
ihsanrabb <ihsanrabbani25@gmail.com>
2020-12-30 12:02:55 +0700

    add redux-persist & create collectionOverview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,14 @@
 import React from 'react'
-import { SHOP_DATA } from './shop.data'
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import CollectionsOverview from '../../components/collection-overview/collection-overview'
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      collection : SHOP_DATA
-    }
-  }
-
-  render () {
-    const {collection} = this.state
-    return (
-      <div className='shop-page'>
-        {collection.map(({id, ...otherCollectionProps}) => (
-          <CollectionPreview key={id} {...otherCollectionProps}/>
-        ))}
-      </div>
-    )
-  }
+const ShopPage = () => {
+  return (
+    <div className='shop-page'>
+      <CollectionsOverview />
+    </div>
+  )
 } 
+
+
 
 export default ShopPage
